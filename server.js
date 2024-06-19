7fa58578034ca20a3de375bb87b360c4862b38a8
Dhameliyahit <153006638+dhameliyahit@users.noreply.github.com>
2024-06-19 15:33:53 +0530

    first commit

--- server.js
+++ server.js
@@ -16,15 +16,17 @@ connectdb()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'./client/build')))
 
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/category',categoryRoute)
 app.use('/api/v1/product',productRoutes)
 
 //home route
-app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+// app.use('*',function(req,res){
+//     res.sendFile(path.join(__dirname,'./client/build/index.html'))
+// })
+app.get('/',(req,res)=>{
+    req.end("<h1>Hello Ecommerce</h2>")
 })
 
 
